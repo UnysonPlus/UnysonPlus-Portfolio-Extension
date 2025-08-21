632c1d58663533527231d699e14808c90e8e3703
jonmlas <jonmlas@gmail.com>
2025-08-21 18:59:44 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -5,18 +5,27 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Portfolio', 'fw' );
-$manifest['description'] = __(
-	'This extension will add a fully fledged portfolio module that will let you display your projects'
-	.' using the built in portfolio pages.',
-	'fw'
+$manifest['slug']        = 'unysonplus-portfolio';
+$manifest['description'] = __( 
+	'This extension will add a fully fledged portfolio module that will let you display your projects using the built in portfolio pages.',
+	'fw' 
 );
-$manifest['version'] = '1.0.9';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Portfolio-Extension';
+$manifest['version']     = '1.0.9';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Portfolio-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/portfolio/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Portfolio-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Portfolio-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
